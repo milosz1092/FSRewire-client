4621c821b1adf75748de72ccde333c5b1bc8189b
Milosz Szewczyk <milosz1092@gmail.com>
2024-01-31 19:11:56 +0100

    refactor: naming

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 mod schema;
 mod utils;
+
 use utils::msfs::check_if_msfs_running;
 use utils::simconnect::update_simconnect_config;
 
 fn main() {
     let is_msfs_running = check_if_msfs_running();
 
-    if is_msfs_running {
-        println!("MSFS is running");
-    } else {
-        println!("MSFS not running");
-    }
+    let update_config_result = update_simconnect_config();
 
-    match update_simconnect_config() {
-        Ok((address, port)) => println!("SimConnect Config - Address: {}, Port: {}", address, port),
-        Err(err) => eprintln!("Error updating SimConnect config: {}", err),
+    if let Err(error) = update_config_result {
+        eprintln!("{}", error);
     }
 }
--- src/utils/simconnect.rs
+++ src/utils/simconnect.rs
@@ -9,8 +9,8 @@ use super::file::{read_windows1252_file, write_windows1252_file};
 
 use crate::schema::simconnect::{SimBaseDocument, SimConnectComm};
 
-static SERVER_ADDR: &str = "0.0.0.0";
-static SERVER_PORT: &str = "500";
+static SIMCONNECT_SERVER_ADDR: &str = "0.0.0.0";
+static SIMCONNECT_SERVER_PORT: &str = "500";
 
 fn get_simconnect_xml_path() -> String {
     if let Some(user_home) = dirs::home_dir() {
@@ -20,22 +20,22 @@ fn get_simconnect_xml_path() -> String {
             .to_str()
             .expect("Failed to convert path to string");
 
-        let xml_file_path_1 = format!(
+        let ms_store_filepath = format!(
             "{}\\AppData\\Local\\Packages\\Microsoft.FlightSimulator_8wekyb3d8bbwe\\LocalCache\\{}",
             user_home_str, sim_connect_file_name
         );
-        let xml_file_path_2 = format!(
+        let steam_edition_filepath = format!(
             "{}\\AppData\\Roaming\\Microsoft Flight Simulator\\{}",
             user_home_str, sim_connect_file_name
         );
 
-        let path_1_exists = std::path::Path::new(&xml_file_path_1).exists();
-        let path_2_exists = std::path::Path::new(&xml_file_path_2).exists();
+        let ms_store_path_exists = std::path::Path::new(&ms_store_filepath).exists();
+        let steam_version_path_exists = std::path::Path::new(&steam_edition_filepath).exists();
 
-        if path_1_exists {
-            xml_file_path_1
-        } else if path_2_exists {
-            xml_file_path_2
+        if ms_store_path_exists {
+            ms_store_filepath
+        } else if steam_version_path_exists {
+            steam_edition_filepath
         } else {
             panic!("Unable to determine SimConnect XML path.");
         }
@@ -52,19 +52,19 @@ pub fn update_simconnect_config() -> Result<(String, String), String> {
     let mut config: SimBaseDocument = xml_from_string(&xml_content)
         .map_err(|e| format!("Error parsing SimConnect.xml: {}", e))?;
 
-    let mut ipv4_address = Some(SERVER_ADDR.to_string());
-    let mut ipv4_port = Some(SERVER_PORT.to_string());
+    let mut ipv4_address = Some(SIMCONNECT_SERVER_ADDR.to_string());
+    let mut ipv4_port = Some(SIMCONNECT_SERVER_PORT.to_string());
     let mut ipv4_found = false;
 
     for comm_section in &mut config.simconnect_comm {
         if comm_section.protocol == "IPv4" && !comm_section.description.contains("Dynamic") {
             ipv4_address = match &comm_section.address {
-                Some(_) => Some(SERVER_ADDR.to_string()),
-                None => Some(SERVER_ADDR.to_string()),
+                Some(_) => Some(SIMCONNECT_SERVER_ADDR.to_string()),
+                None => Some(SIMCONNECT_SERVER_ADDR.to_string()),
             };
             ipv4_port = match &comm_section.port {
                 Some(port) => Some(port.clone()),
-                None => Some(SERVER_PORT.to_string()),
+                None => Some(SIMCONNECT_SERVER_PORT.to_string()),
             };
 
             comm_section.address = ipv4_address.clone();
