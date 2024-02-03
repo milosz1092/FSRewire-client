bf6a2b32283083eeab9963cc82c0b110843c4c71
Milosz Szewczyk <milosz1092@gmail.com>
2024-02-03 16:58:24 +0100

    feat: wgpu rendering

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,75 @@ use winit::{
     dpi::{LogicalPosition, PhysicalPosition, PhysicalSize, Position},
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoopBuilder},
-    window::{Theme, Window, WindowAttributes, WindowBuilder, WindowButtons},
+    window::{Theme, Window, WindowAttributes, WindowBuilder, WindowButtons, WindowId},
 };
 
 static APP_TITLE: &str = "FSRewire-client";
 
+async fn render(window: &Window) {
+    let instance = wgpu::Instance::default();
+    let viewport = instance.create_surface(&window).unwrap();
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            // Request an adapter which can render to our surface
+            compatible_surface: Some(&viewport),
+            ..Default::default()
+        })
+        .await
+        .expect("Failed to find an appropriate adapter");
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::downlevel_defaults(),
+            },
+            None,
+        )
+        .await
+        .expect("Failed to create device");
+
+    let size = window.inner_size();
+    let config = viewport
+        .get_default_config(&adapter, size.width, size.height)
+        .unwrap();
+
+    viewport.configure(&device, &config);
+
+    let frame = viewport.get_current_texture().unwrap();
+
+    let view = frame
+        .texture
+        .create_view(&wgpu::TextureViewDescriptor::default());
+
+    let mut encoder =
+        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+    {
+        let _rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: None,
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+    }
+
+    queue.submit(Some(encoder.finish()));
+    frame.present();
+}
+
 fn main() {
+    env_logger::init();
+
     let is_msfs_running = check_if_msfs_running();
 
     let event_loop = EventLoopBuilder::new().build().unwrap();
@@ -46,6 +109,8 @@ fn main() {
         .with_enabled_buttons(WindowButtons::MINIMIZE.union(WindowButtons::CLOSE))
         .build(&event_loop)
         .unwrap();
+
+    pollster::block_on(render(&window));
 
     window.set_visible(true);
     window.focus_window();
@@ -88,17 +153,17 @@ fn main() {
         }
     }
 
-    event_loop.run(move |window_event, event_loop| {
+    event_loop.run(move |event: Event<()>, event_loop| {
         event_loop.set_control_flow(ControlFlow::Wait);
 
-        match window_event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                window.set_visible(false);
+        if let Event::WindowEvent { window_id, event } = event {
+            match event {
+                WindowEvent::CloseRequested => {
+                    window.set_visible(false);
+                }
+                WindowEvent::RedrawRequested => {}
+                _ => {}
             }
-            _ => {}
         }
 
         if let Ok(event) = menu_channel.try_recv() {
