2f6877761eb6ad9e376a7f78bf5a3278ee53af37
Milosz Szewczyk <milosz1092@gmail.com>
2024-02-05 19:49:57 +0100

    fix: refactor

--- src/main.rs
+++ src/main.rs
@@ -49,129 +49,6 @@ impl AppState {
 
 fn get_text_renderer(device: &Device, queue: &Queue, swapchain_format: TextureFormat) {}
 
-fn redraw(
-    window: &Window,
-    mut font_system: FontSystem,
-    mut text_renderer: TextRenderer,
-    mut text_atlas: TextAtlas,
-    device: &Device,
-    queue: &Queue,
-    viewport: &Surface,
-    mut swash_cache: SwashCache,
-) {
-    let physical_width = window.inner_size().width;
-    let physical_height = window.inner_size().height;
-
-    let mut text_first = Buffer::new(&mut font_system, Metrics::new(20.0, 42.0));
-    let mut text_second = Buffer::new(&mut font_system, Metrics::new(14.0, 42.0));
-
-    text_first.set_size(
-        &mut font_system,
-        physical_width as f32,
-        physical_height as f32,
-    );
-    text_first.set_text(
-        &mut font_system,
-        "Hello world! 👋",
-        Attrs::new().family(Family::SansSerif),
-        Shaping::Advanced,
-    );
-    text_first.shape_until_scroll(&mut font_system);
-
-    text_second.set_size(
-        &mut font_system,
-        physical_width as f32,
-        physical_height as f32,
-    );
-    text_second.set_text(
-        &mut font_system,
-        "ver 1.0.3",
-        Attrs::new().family(Family::Monospace),
-        Shaping::Advanced,
-    );
-    text_second.shape_until_scroll(&mut font_system);
-
-    text_renderer
-        .prepare(
-            &device,
-            &queue,
-            &mut font_system,
-            &mut text_atlas,
-            Resolution {
-                width: physical_width,
-                height: physical_height,
-            },
-            [
-                TextArea {
-                    buffer: &text_first,
-                    left: 0.0,
-                    top: 0.0,
-                    scale: 1.0,
-                    bounds: TextBounds {
-                        left: 0,
-                        top: 0,
-                        right: physical_width as i32,
-                        bottom: physical_height as i32,
-                    },
-                    default_color: Color::rgb(220, 220, 220),
-                },
-                TextArea {
-                    buffer: &text_second,
-                    left: 520.0,
-                    top: 270.0,
-                    scale: 1.0,
-                    bounds: TextBounds {
-                        left: 0,
-                        top: 0,
-                        right: physical_width as i32,
-                        bottom: physical_height as i32,
-                    },
-                    default_color: Color::rgb(100, 100, 100),
-                },
-            ],
-            &mut swash_cache,
-        )
-        .unwrap();
-
-    let frame = viewport.get_current_texture().unwrap();
-
-    let view = frame
-        .texture
-        .create_view(&wgpu::TextureViewDescriptor::default());
-
-    let mut encoder =
-        device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-
-    {
-        let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-            label: None,
-            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                view: &view,
-                resolve_target: None,
-                ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Clear(wgpu::Color {
-                        r: 0.02,
-                        g: 0.02,
-                        b: 0.02,
-                        a: 1.0,
-                    }),
-                    store: wgpu::StoreOp::Store,
-                },
-            })],
-            depth_stencil_attachment: None,
-            timestamp_writes: None,
-            occlusion_query_set: None,
-        });
-
-        text_renderer.render(&text_atlas, &mut rpass).unwrap();
-    }
-
-    queue.submit(Some(encoder.finish()));
-    frame.present();
-
-    text_atlas.trim();
-}
-
 async fn run(window: &Window, app_state: &AppState, event_loop: EventLoop<()>) {
     let mut system_try = SystemTry::new();
 
@@ -186,16 +63,121 @@ async fn run(window: &Window, app_state: &AppState, event_loop: EventLoop<()>) {
         mut text_renderer,
     ) = configure_wgpu(window).await;
 
-    redraw(
-        window,
-        font_system,
-        text_renderer,
-        text_atlas,
-        &device,
-        &queue,
-        &viewport,
-        swash_cache,
-    );
+    let mut redraw = || {
+        let physical_width = window.inner_size().width;
+        let physical_height = window.inner_size().height;
+
+        let mut text_first = Buffer::new(&mut font_system, Metrics::new(20.0, 42.0));
+        let mut text_second = Buffer::new(&mut font_system, Metrics::new(14.0, 42.0));
+
+        text_first.set_size(
+            &mut font_system,
+            physical_width as f32,
+            physical_height as f32,
+        );
+        text_first.set_text(
+            &mut font_system,
+            "Hello world! 👋",
+            Attrs::new().family(Family::SansSerif),
+            Shaping::Advanced,
+        );
+        text_first.shape_until_scroll(&mut font_system);
+
+        text_second.set_size(
+            &mut font_system,
+            physical_width as f32,
+            physical_height as f32,
+        );
+        text_second.set_text(
+            &mut font_system,
+            "ver 1.0.3",
+            Attrs::new().family(Family::Monospace),
+            Shaping::Advanced,
+        );
+        text_second.shape_until_scroll(&mut font_system);
+
+        text_renderer
+            .prepare(
+                &device,
+                &queue,
+                &mut font_system,
+                &mut text_atlas,
+                Resolution {
+                    width: physical_width,
+                    height: physical_height,
+                },
+                [
+                    TextArea {
+                        buffer: &text_first,
+                        left: 0.0,
+                        top: 0.0,
+                        scale: 1.0,
+                        bounds: TextBounds {
+                            left: 0,
+                            top: 0,
+                            right: physical_width as i32,
+                            bottom: physical_height as i32,
+                        },
+                        default_color: Color::rgb(220, 220, 220),
+                    },
+                    TextArea {
+                        buffer: &text_second,
+                        left: 520.0,
+                        top: 270.0,
+                        scale: 1.0,
+                        bounds: TextBounds {
+                            left: 0,
+                            top: 0,
+                            right: physical_width as i32,
+                            bottom: physical_height as i32,
+                        },
+                        default_color: Color::rgb(100, 100, 100),
+                    },
+                ],
+                &mut swash_cache,
+            )
+            .unwrap();
+
+        let frame = viewport.get_current_texture().unwrap();
+
+        let view = frame
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder =
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        {
+            let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: None,
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.02,
+                            g: 0.02,
+                            b: 0.02,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                timestamp_writes: None,
+                occlusion_query_set: None,
+            });
+
+            text_renderer.render(&text_atlas, &mut rpass).unwrap();
+        }
+
+        queue.submit(Some(encoder.finish()));
+        frame.present();
+
+        text_atlas.trim();
+    };
+
+    redraw();
 
     window.set_visible(true);
     window.focus_window();
